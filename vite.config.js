91680aac664497f397ffbab0187024810b05a060
fokasiyy <foka123zate@gmail.con>
2025-06-06 15:31:08 +0300

    Setup GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
+
+
 import { defineConfig } from 'vite';
+import FullReload from 'vite-plugin-full-reload';
+
 
 export default defineConfig({
-  base: '/goit-js-hw-09/', // ТУТ ВИКОРИСТАЙ НАЗВУ СВОГО РЕПОЗИТОРІЮ
+  base: '/goit-js-hw-09/', 
+  root: 'src',             
+  build: {
+    outDir: '../dist',     
+    emptyOutDir: true,     
+  },
+  plugins: [
+    FullReload(['src/**/*.html']), 
+  ],
 });
