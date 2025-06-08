a73aa7faee751a9169afac5f27ea9942a057468d
fokasiyy <foka123zate@gmail.con>
2025-06-08 17:06:04 +0300

    Fix SimpleLightbox config: remove invalid 'captions' option

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,45 +1,95 @@
-const formEl = document.querySelector('.feedback-form');
-const emailInput = document.querySelector('input[name="email"]');
-const messageInput = document.querySelector('textarea[name="message"]');
-const STORAGE_KEY = 'feedback-form-state';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let formData = {};
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
 
-loadFormData();
+const ul = document.querySelector('.gallery');
 
-formEl.addEventListener('input', e => {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-});
+let markUp = '';
 
-formEl.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const email = emailInput.value.trim();
-  const message = messageInput.value.trim();
-
-  if (email === '' || message === '') {
-    alert('Please fill in all fields!');
-    return;
-  }
-
-  console.log('Form submitted:', { email, message });
-
-  formEl.reset();
-  localStorage.removeItem(STORAGE_KEY);
-  formData = {};
-});
-
-function loadFormData() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}');
-
-  if (savedData.email) {
-    emailInput.value = savedData.email;
-    formData.email = savedData.email;
-  }
-
-  if (savedData.message) {
-    messageInput.value = savedData.message;
-    formData.message = savedData.message;
-  }
+for (let i = 0; i < images.length; i++) {
+  markUp += `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${images[i].original}">
+        <img 
+          class="gallery-image" 
+          src="${images[i].preview}" 
+          alt="${images[i].description}" 
+        />
+      </a>
+    </li>
+  `;
 }
+
+ul.insertAdjacentHTML('afterbegin', markUp);
+
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
